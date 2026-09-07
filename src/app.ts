9906a19d94fcc3d856651d8f7dba4d30895402a1
Fovurt <rebrov_08@bk.ru>
2026-09-07 11:32:27 +0400

    test api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
+
+const port = 8000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(
+    JSON.stringify({
+      message: "Тест АПИ! Привет!",
+      timestamp: new Date().toISOString(),
+    })
+  );
+});
+
+server.listen(port, () => {
+  console.log(`Сервер запущен на порту http://localhost:${port}`);
+});
